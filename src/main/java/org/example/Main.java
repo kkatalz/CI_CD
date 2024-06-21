2841fee2efbc4fb864bac428d6f24341029f21c9
kkatalz <zlata.kiev2004@gmail.com>
2024-06-21 14:56:57 +0300

    Fixed checkstyles errors

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+/**
+ * Main class of the application.
+ */
+public final class  Main {
+
+    private Main() {
+    }
+
+    /**
+     * Main method which serves as the entry point of the application.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(final String[] args) {
         System.out.println("Hello world!");
     }
 }
